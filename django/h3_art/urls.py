1beb0fe6615130d587e24c240492dd928ee6eef3
Stanislav Matsevich <smatsevich@gmail.com>
2012-04-08 14:01:50 +0300

    admin activated, TZ switched to Europe/Kiev

--- django/h3_art/urls.py
+++ django/h3_art/urls.py
@@ -2,11 +2,12 @@ from h3_art import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from gifts import views
+from gifts import views as gifts_views
+from users import views as users_views
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -14,13 +15,13 @@ urlpatterns = patterns('',
     # url(r'^h3_art/', include('h3_art.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.main),
-    url(r'^gifts/new/([A-Za-z]*)$', views.new_gift),
-    url(r'^gifts/$', views.user_gifts),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', gifts_views.main),
+    url(r'^gifts/new/([A-Za-z]*)$', gifts_views.new_gift),
+    url(r'^gifts/$', gifts_views.user_gifts),
 )
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
