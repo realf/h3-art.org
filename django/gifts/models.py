038a1ec7f58d4e222764de1cb2cc8badb565d4aa
Stanislav Matsevich <matsevich.devel@gmail.com>
2012-05-18 16:16:54 +0300

    model for artifact added

--- django/gifts/models.py
+++ django/gifts/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import User
+#import string
+#import random
+#import hashlib
 
 # Create your models here.
+
+def rnd_str(rnd_len = 6):
+    import string, random
+    
+    choice_set = string.ascii_letters + string.digits
+    return "".join([random.choice(choice_set) for i in range(rnd_len)])
+
+
+class Location(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+        
+    def __unicode__(self):
+        return u"(%.8f; %.8f)" %(self.latitude, self.longitude)
+        
+    
+class Quest(models.Model):
+    quest_type = models.CharField(max_length = 20)
+    
+    def __unicode__(self):
+        return self.quest_type
+        
+        
+class Permissions(models.Model):
+    name = models.CharField(max_length = 20)
+    
+    def __unicode__(self):
+        return self.name
+
+
+class Artifact(models.Model):
+    name = models.CharField(max_length = 50)
+    description = models.TextField(blank = True)
+    uid = models.CharField(max_length = 16, unique = True, 
+                                db_index = True, editable = False,
+                                default = rnd_str)
+    photo = models.ImageField(upload_to = "/gifts/photos", blank = True)
+    creator = models.ManyToManyField(User, related_name = 'created_gifts') #origin created the gift
+    master = models.ManyToManyField(User, related_name = 'mastered_gifts') #master has rights and ratings of the gift
+    owner = models.ForeignKey(User, related_name = 'owned_gifts') #owner is person, who has got the gift by last transaction
+    create_datetime = models.DateTimeField(auto_now_add=True)
+    create_location = models.ForeignKey(Location, blank=True, null=True, related_name = '+') #location field type will be specified after learning geolocation
+    lastpass_datetime = models.DateTimeField(blank=True) #not required. It's possible to get it from transactions 
+    lastpass_location = models.ForeignKey(Location, blank=True,  null=True, related_name = '+') #may be obtained from transactions + location type = ?    
+    quest = models.ForeignKey(Quest, blank=True, null=True, related_name = '+')
+    permissions = models.ForeignKey(Permissions, related_name = '+')
+    ratings = models.IntegerField(default=0)
+    abuses = models.IntegerField(default=0)
+    status = models.IntegerField(default=1)
+    
+    def __unicode__(self):
+        return self.name
+        
+
+        
--- django/gifts/views.py
+++ django/gifts/views.py
@@ -1,13 +1,34 @@
 # Create your views here.
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+#from django.contrib.sessions.models import Session
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from gifts.forms import GiftCreateForm
+from gifts.models import Artifact
 
+@login_required
+def user_profile(request):
+#    user = request.user
+    return render_to_response('profile.html', {}, 
+                              context_instance=RequestContext(request))    
+    
+
+@login_required
 def new_gift(request, gift = ""):
     if request.method == 'POST':
         form = GiftCreateForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            user = User.objects.get(username=request.user.username)
+            artifact = Artifact(name=cd['name'], 
+                                description = cd['description'],
+                                creator = user,
+                                master = user,
+                                owner = request.user,
+                                status = 1)
+            artifact.save()
             return redirect('/gifts/new/'+cd['name'])
     else:
         form = GiftCreateForm
@@ -15,8 +36,13 @@ def new_gift(request, gift = ""):
                               context_instance=RequestContext(request))
     
 def user_gifts(request):
+    
     return render_to_response('user_gifts.html', {}, context_instance=RequestContext(request))
 
+
+def my_gifts(request):
+    return render_to_response('my_gifts.html', {}, context_instance=RequestContext(request))
+    
 
 def main(request):
     return render_to_response('index.html', {}, context_instance=RequestContext(request))  
