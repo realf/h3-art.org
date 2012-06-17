ac4e56e32e7dba2443484b3a1a90d622aef2c02a
Stanislav Matsevich <matsevich.devel@gmail.com>
2012-06-18 02:07:23 +0300

    add custom_settings.py

---
+++ django/h3_art/custom_settings.py
@@ -0,0 +1,57 @@
+import os.path
+
+_PATH = os.path.join(os.path.dirname(__file__), "..")
+
+
+# Databases config
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(_PATH, '..', 'db', 'sqlite.db'),    # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = os.path.join(_PATH, '..', 'media').replace('\\', '/')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(_PATH, '..', 'static').replace('\\', '/')
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    #os.path.join(_PATH, '..', 'ui').replace('\\', '/'),
+    os.path.join(_PATH, '..', 'static').replace('\\', '/'),
+)
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(_PATH, 'templates').replace('\\', '/'),
+)
+
+
+
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '1r20k74g56(nm7gm*w#7a7pcm9!$u%d)o8^jgxxe*c+-7o1-xp'
