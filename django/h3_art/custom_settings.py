2049522264186bb3985806199eecbfbf9ea54d6f
Stanislav Matsevich <matsevich.devel@gmail.com>
2012-06-18 02:56:56 +0300

    fix custom_settings.py: static, media files

--- django/h3_art/custom_settings.py
+++ django/h3_art/custom_settings.py
@@ -17,7 +17,7 @@ DATABASES = {
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(_PATH, '..', 'media').replace('\\', '/')
+MEDIA_ROOT = os.path.join(_PATH, 'files', 'media').replace('\\', '/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -28,7 +28,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(_PATH, '..', 'static').replace('\\', '/')
+STATIC_ROOT = os.path.join(_PATH, 'files', 'static').replace('\\', '/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
